ec0afd88e1ad8a5eba67b0d8e56d3e56fa2addb7
gpskwlkr <giorgianakidze@outlook.com>
2023-04-13 22:35:48 +0400

    added warp functionality to powershell

--- windows/powershell_profile.ps1
+++ windows/powershell_profile.ps1
@@ -7,23 +7,24 @@ using namespace System.Management.Automation.Language
 oh-my-posh init pwsh --config 'C:\Users\giorg\AppData\Local\Programs\oh-my-posh\themes\craver.omp.json' | Invoke-Expression
 Import-Module -Name Terminal-Icons
 Import-Module PSReadLine
+Import-Module PoShWarp
 
 Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
     param($wordToComplete, $commandAst, $cursorPosition)
         [Console]::InputEncoding = [Console]::OutputEncoding = $OutputEncoding = [System.Text.Utf8Encoding]::new()
         $Local:word = $wordToComplete.Replace('"', '""')
-        $Local:ast = $commandAst.ToString().Replace('"', '""')
-        winget complete --word="$Local:word" --commandline "$Local:ast" --position $cursorPosition | ForEach-Object {
-            [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
-        }
+            $Local:ast = $commandAst.ToString().Replace('"', '""')
+            winget complete --word="$Local:word" --commandline "$Local:ast" --position $cursorPosition | ForEach-Object {
+                [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+            }
 }
 
 Register-ArgumentCompleter -Native -CommandName dotnet -ScriptBlock {
-     param($commandName, $wordToComplete, $cursorPosition)
-         dotnet complete --position $cursorPosition "$wordToComplete" | ForEach-Object {
+    param($commandName, $wordToComplete, $cursorPosition)
+        dotnet complete --position $cursorPosition "$wordToComplete" | ForEach-Object {
             [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
-         }
- }
+        }
+}
 
 # PSReadline settings
 
@@ -33,163 +34,197 @@ Set-PSReadLineOption -EditMode Windows
 
 # Some custom .NET commands 
 Set-PSReadLineKeyHandler -Key Ctrl+Shift+b `
-                         -BriefDescription BuildCurrentDirectory `
-                         -LongDescription "Build the current directory" `
-                         -ScriptBlock {
+-BriefDescription BuildCurrentDirectory `
+-LongDescription "Build the current directory" `
+-ScriptBlock {
     [Microsoft.PowerShell.PSConsoleReadLine]::RevertLine()
-    [Microsoft.PowerShell.PSConsoleReadLine]::Insert("dotnet build")
-    [Microsoft.PowerShell.PSConsoleReadLine]::AcceptLine()
+        [Microsoft.PowerShell.PSConsoleReadLine]::Insert("dotnet build")
+            [Microsoft.PowerShell.PSConsoleReadLine]::AcceptLine()
 }
 
 Set-PSReadLineKeyHandler -Key Ctrl+Alt+t `
-                         -BriefDescription BuildCurrentDirectory `
-                         -LongDescription "Run tests for current directory" `
-                         -ScriptBlock {
+-BriefDescription BuildCurrentDirectory `
+-LongDescription "Run tests for current directory" `
+-ScriptBlock {
     [Microsoft.PowerShell.PSConsoleReadLine]::RevertLine()
-    [Microsoft.PowerShell.PSConsoleReadLine]::Insert("dotnet test")
-    [Microsoft.PowerShell.PSConsoleReadLine]::AcceptLine()
+        [Microsoft.PowerShell.PSConsoleReadLine]::Insert("dotnet test")
+            [Microsoft.PowerShell.PSConsoleReadLine]::AcceptLine()
 }
 
 Set-PSReadLineKeyHandler -Key F1 `
-                         -BriefDescription CommandHelp `
-                         -LongDescription "Open the help window for the current command" `
-                         -ScriptBlock {
+-BriefDescription CommandHelp `
+-LongDescription "Open the help window for the current command" `
+-ScriptBlock {
     param($key, $arg)
 
-    $ast = $null
-    $tokens = $null
-    $errors = $null
-    $cursor = $null
-    [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$ast, [ref]$tokens, [ref]$errors, [ref]$cursor)
+        $ast = $null
+        $tokens = $null
+        $errors = $null
+        $cursor = $null
+        [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$ast, [ref]$tokens, [ref]$errors, [ref]$cursor)
 
-    $commandAst = $ast.FindAll( {
-        $node = $args[0]
-        $node -is [CommandAst] -and
-            $node.Extent.StartOffset -le $cursor -and
-            $node.Extent.EndOffset -ge $cursor
-        }, $true) | Select-Object -Last 1
+        $commandAst = $ast.FindAll( {
+                $node = $args[0]
+                $node -is [CommandAst] -and
+                $node.Extent.StartOffset -le $cursor -and
+                $node.Extent.EndOffset -ge $cursor
+                }, $true) | Select-Object -Last 1
 
     if ($commandAst -ne $null)
     {
         $commandName = $commandAst.GetCommandName()
-        if ($commandName -ne $null)
-        {
-            $command = $ExecutionContext.InvokeCommand.GetCommand($commandName, 'All')
-            if ($command -is [AliasInfo])
-            {
-                $commandName = $command.ResolvedCommandName
-            }
-
             if ($commandName -ne $null)
             {
-                Get-Help $commandName -ShowWindow
+                $command = $ExecutionContext.InvokeCommand.GetCommand($commandName, 'All')
+                    if ($command -is [AliasInfo])
+                    {
+                        $commandName = $command.ResolvedCommandName
+                    }
+
+                if ($commandName -ne $null)
+                {
+                    Get-Help $commandName -ShowWindow
+                }
             }
-        }
     }
 }
 
 Set-PSReadLineKeyHandler -Key '"',"'" `
-                         -BriefDescription SmartInsertQuote `
-                         -LongDescription "Insert paired quotes if not already on a quote" `
-                         -ScriptBlock {
+-BriefDescription SmartInsertQuote `
+-LongDescription "Insert paired quotes if not already on a quote" `
+-ScriptBlock {
     param($key, $arg)
 
-    $quote = $key.KeyChar
+        $quote = $key.KeyChar
 
-    $selectionStart = $null
-    $selectionLength = $null
-    [Microsoft.PowerShell.PSConsoleReadLine]::GetSelectionState([ref]$selectionStart, [ref]$selectionLength)
+        $selectionStart = $null
+        $selectionLength = $null
+        [Microsoft.PowerShell.PSConsoleReadLine]::GetSelectionState([ref]$selectionStart, [ref]$selectionLength)
 
-    $line = $null
-    $cursor = $null
-    [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$line, [ref]$cursor)
+        $line = $null
+            $cursor = $null
+            [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$line, [ref]$cursor)
 
-    # If text is selected, just quote it without any smarts
-    if ($selectionStart -ne -1)
-    {
-        [Microsoft.PowerShell.PSConsoleReadLine]::Replace($selectionStart, $selectionLength, $quote + $line.SubString($selectionStart, $selectionLength) + $quote)
-        [Microsoft.PowerShell.PSConsoleReadLine]::SetCursorPosition($selectionStart + $selectionLength + 2)
-        return
-    }
+# If text is selected, just quote it without any smarts
+            if ($selectionStart -ne -1)
+            {
+                [Microsoft.PowerShell.PSConsoleReadLine]::Replace($selectionStart, $selectionLength, $quote + $line.SubString($selectionStart, $selectionLength) + $quote)
+                    [Microsoft.PowerShell.PSConsoleReadLine]::SetCursorPosition($selectionStart + $selectionLength + 2)
+                        return
+            }
 
     $ast = $null
-    $tokens = $null
-    $parseErrors = $null
-    [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$ast, [ref]$tokens, [ref]$parseErrors, [ref]$null)
+        $tokens = $null
+        $parseErrors = $null
+        [Microsoft.PowerShell.PSConsoleReadLine]::GetBufferState([ref]$ast, [ref]$tokens, [ref]$parseErrors, [ref]$null)
 
-    function FindToken
-    {
-        param($tokens, $cursor)
-
-        foreach ($token in $tokens)
+        function FindToken
         {
-            if ($cursor -lt $token.Extent.StartOffset) { continue }
-            if ($cursor -lt $token.Extent.EndOffset) {
-                $result = $token
-                $token = $token -as [StringExpandableToken]
-                if ($token) {
-                    $nested = FindToken $token.NestedTokens $cursor
-                    if ($nested) { $result = $nested }
-                }
+            param($tokens, $cursor)
 
-                return $result
-            }
+                foreach ($token in $tokens)
+                {
+                    if ($cursor -lt $token.Extent.StartOffset) { continue }
+                    if ($cursor -lt $token.Extent.EndOffset) {
+                        $result = $token
+                            $token = $token -as [StringExpandableToken]
+                            if ($token) {
+                                $nested = FindToken $token.NestedTokens $cursor
+                                    if ($nested) { $result = $nested }
+                            }
+
+                        return $result
+                    }
+                }
+            return $null
         }
-        return $null
-    }
 
     $token = FindToken $tokens $cursor
 
-    # If we're on or inside a **quoted** string token (so not generic), we need to be smarter
-    if ($token -is [StringToken] -and $token.Kind -ne [TokenKind]::Generic) {
-        # If we're at the start of the string, assume we're inserting a new string
-        if ($token.Extent.StartOffset -eq $cursor) {
-            [Microsoft.PowerShell.PSConsoleReadLine]::Insert("$quote$quote ")
-            [Microsoft.PowerShell.PSConsoleReadLine]::SetCursorPosition($cursor + 1)
-            return
-        }
+# If we're on or inside a **quoted** string token (so not generic), we need to be smarter
+        if ($token -is [StringToken] -and $token.Kind -ne [TokenKind]::Generic) {
+# If we're at the start of the string, assume we're inserting a new string
+            if ($token.Extent.StartOffset -eq $cursor) {
+                [Microsoft.PowerShell.PSConsoleReadLine]::Insert("$quote$quote ")
+                    [Microsoft.PowerShell.PSConsoleReadLine]::SetCursorPosition($cursor + 1)
+                        return
+            }
 
-        # If we're at the end of the string, move over the closing quote if present.
-        if ($token.Extent.EndOffset -eq ($cursor + 1) -and $line[$cursor] -eq $quote) {
-            [Microsoft.PowerShell.PSConsoleReadLine]::SetCursorPosition($cursor + 1)
-            return
+# If we're at the end of the string, move over the closing quote if present.
+            if ($token.Extent.EndOffset -eq ($cursor + 1) -and $line[$cursor] -eq $quote) {
+                [Microsoft.PowerShell.PSConsoleReadLine]::SetCursorPosition($cursor + 1)
+                    return
+            }
         }
-    }
 
     if ($null -eq $token -or
-        $token.Kind -eq [TokenKind]::RParen -or $token.Kind -eq [TokenKind]::RCurly -or $token.Kind -eq [TokenKind]::RBracket) {
+            $token.Kind -eq [TokenKind]::RParen -or $token.Kind -eq [TokenKind]::RCurly -or $token.Kind -eq [TokenKind]::RBracket) {
         if ($line[0..$cursor].Where{$_ -eq $quote}.Count % 2 -eq 1) {
-            # Odd number of quotes before the cursor, insert a single quote
+# Odd number of quotes before the cursor, insert a single quote
             [Microsoft.PowerShell.PSConsoleReadLine]::Insert($quote)
         }
         else {
-            # Insert matching quotes, move cursor to be in between the quotes
+# Insert matching quotes, move cursor to be in between the quotes
             [Microsoft.PowerShell.PSConsoleReadLine]::Insert("$quote$quote")
-            [Microsoft.PowerShell.PSConsoleReadLine]::SetCursorPosition($cursor + 1)
+                [Microsoft.PowerShell.PSConsoleReadLine]::SetCursorPosition($cursor + 1)
         }
         return
     }
 
-    # If cursor is at the start of a token, enclose it in quotes.
+# If cursor is at the start of a token, enclose it in quotes.
     if ($token.Extent.StartOffset -eq $cursor) {
         if ($token.Kind -eq [TokenKind]::Generic -or $token.Kind -eq [TokenKind]::Identifier -or 
-            $token.Kind -eq [TokenKind]::Variable -or $token.TokenFlags.hasFlag([TokenFlags]::Keyword)) {
+                $token.Kind -eq [TokenKind]::Variable -or $token.TokenFlags.hasFlag([TokenFlags]::Keyword)) {
             $end = $token.Extent.EndOffset
-            $len = $end - $cursor
-            [Microsoft.PowerShell.PSConsoleReadLine]::Replace($cursor, $len, $quote + $line.SubString($cursor, $len) + $quote)
-            [Microsoft.PowerShell.PSConsoleReadLine]::SetCursorPosition($end + 2)
-            return
+                $len = $end - $cursor
+                [Microsoft.PowerShell.PSConsoleReadLine]::Replace($cursor, $len, $quote + $line.SubString($cursor, $len) + $quote)
+                [Microsoft.PowerShell.PSConsoleReadLine]::SetCursorPosition($end + 2)
+                    return
         }
     }
 
-    # We failed to be smart, so just insert a single quote
+# We failed to be smart, so just insert a single quote
     [Microsoft.PowerShell.PSConsoleReadLine]::Insert($quote)
 }
 
 function gitCmt {
     param([string]$mes = "Default commit message")
 
-    git add .
-    git commit -m $mes
-    git push
+        git add .
+        git commit -m $mes
+        git push
+}
+
+# Create a new warp point in the current directoryr
+function wdc {
+# name - warp point name
+    param([string]$name) 
+
+        $CurrentDirectory = Get-Location
+
+        New-WarpLocation $name $CurrentDirectory
+}
+
+# Create a new warp point providing the directory it warps you to
+function wdcd {
+# name - warp point name
+# directory - path to the directory
+    param([string]$name, [string]$directory)
+
+        New-WarpLocation $name $directory
+}
+
+# Go to created warp directory
+function wdg {
+# name - warp point name
+    param([string]$name)
+
+        Select-WarpLocation $name
+}
+
+function wdd {
+# name - warp point name
+    param([string]$name)
+
+        Remove-WarpLocation $name
 }
