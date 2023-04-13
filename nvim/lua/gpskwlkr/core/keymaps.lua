4c2fe2782d704960dd3de42709c89b020d47abb1
gpskwlkr <giorgianakidze@outlook.com>
2023-04-13 19:41:41 +0400

    nvim keymap for fixing indentation in whole file

--- nvim/lua/gpskwlkr/core/keymaps.lua
+++ nvim/lua/gpskwlkr/core/keymaps.lua
@@ -21,3 +21,4 @@ keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close current tab
 keymap.set("n", "<leader>tn", ":tabn<CR>") --  go to next tab
 keymap.set("n", "<leader>tp", ":tabp<CR>") --  go to previous tab
 
+keymap.set("n", "<leader>ff", "=G<CR>") -- fix formatting in file
