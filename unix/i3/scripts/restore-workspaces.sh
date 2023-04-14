707f81326b2cc8c86ec5a39b8aa36b7b6cc63290
gpskwlkr <giorgianakidze@outlook.com>
2023-04-15 02:58:01 +0400

    update configs

--- unix/i3/scripts/restore-workspaces.sh
+++ unix/i3/scripts/restore-workspaces.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-i3-resurrect restore -w "1: Editor "
+i3-resurrect restore -w "1: Editor "
 i3-resurrect restore -w "2: Internet "
-i3-resurrect restore -w "4: Htop "
+i3-resurrect restore -w "4: Htop "
 i3-resurrect restore -w "5: Games "
 i3-resurrect restore -w "10: Social/Music "
