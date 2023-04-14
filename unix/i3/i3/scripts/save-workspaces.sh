2e3edf02097a1b14e7000e9d4ef3aa776739877c
gpskwlkr <giorgianakidze@outlook.com>
2023-04-14 18:29:01 +0400

    i3 config update

---
+++ unix/i3/i3/scripts/save-workspaces.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+i3-resurrect save -w "1: Editor "
+i3-resurrect save -w "2: Internet "
+i3-resurrect save -w "4: Htop "
+i3-resurrect save -w "5: Games "
+i3-resurrect save -w "10: Social/Music "
