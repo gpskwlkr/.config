cfe7a983223a439fd54d38445ef26b0a8f84d363
gpskwlkr <giorgianakidze@outlook.com>
2023-04-15 14:20:02 +0400

    nvim dap

--- nvim/lua/gpskwlkr/plugins-setup.lua
+++ nvim/lua/gpskwlkr/plugins-setup.lua
@@ -13,63 +13,63 @@ return require('packer').startup(function(use)
         'rose-pine/neovim',
         as = 'rose-pine'
     })
-    
+
     use("theprimeagen/harpoon")
     use({"nvim-treesitter/nvim-treesitter", run = function()
         require("nvim-treesitter.install").update({ with_sync = true })
     end,
-    })   
+})   
 
-    use("christoomey/vim-tmux-navigator") -- tmux & split window navigation
+use("christoomey/vim-tmux-navigator") -- tmux & split window navigation
 
-    use("szw/vim-maximizer") -- maximizes and restores current window
+use("szw/vim-maximizer") -- maximizes and restores current window
 
-    -- essential plugins
-    use("tpope/vim-surround") -- add, delete, change surroundings (it's awesome)
-    use("inkarkat/vim-ReplaceWithRegister") -- replace with register contents using motion (gr + motion)
+-- essential plugins
+use("tpope/vim-surround") -- add, delete, change surroundings (it's awesome)
+use("inkarkat/vim-ReplaceWithRegister") -- replace with register contents using motion (gr + motion)
 
-    -- commenting with gc
-    use("numToStr/Comment.nvim")
+-- commenting with gc
+use("numToStr/Comment.nvim")
 
-    -- file explorer
-    use("nvim-tree/nvim-tree.lua")
+-- file explorer
+use("nvim-tree/nvim-tree.lua")
 
-    -- vs-code like icons
-    use("nvim-tree/nvim-web-devicons")
+-- vs-code like icons
+use("nvim-tree/nvim-web-devicons")
 
-    -- statusline
-    use("nvim-lualine/lualine.nvim")
-    -- auto closing
-    use("windwp/nvim-autopairs") -- autoclose parens, brackets, quotes, etc...
-    use({ "windwp/nvim-ts-autotag", after = "nvim-treesitter" }) -- autoclose tags
+-- statusline
+use("nvim-lualine/lualine.nvim")
+-- auto closing
+use("windwp/nvim-autopairs") -- autoclose parens, brackets, quotes, etc...
+use({ "windwp/nvim-ts-autotag", after = "nvim-treesitter" }) -- autoclose tags
 
-    -- git integration
-    use("lewis6991/gitsigns.nvim") -- show line modifications on left hand side
+-- git integration
+use("lewis6991/gitsigns.nvim") -- show line modifications on left hand side
 
-    -- autocompletion
-    use("hrsh7th/nvim-cmp") -- completion plugin
-    use("hrsh7th/cmp-buffer") -- source for text in buffer
-    use("hrsh7th/cmp-path") -- source for file system paths
+-- autocompletion
+use("hrsh7th/nvim-cmp") -- completion plugin
+use("hrsh7th/cmp-buffer") -- source for text in buffer
+use("hrsh7th/cmp-path") -- source for file system paths
 
-    use {
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v2.x',
-        requires = {
+use {
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v2.x',
+    requires = {
         -- LSP Support
         {'neovim/nvim-lspconfig'},             -- Required
         {                                      -- Optional
-          'williamboman/mason.nvim',
-          run = function()
+        'williamboman/mason.nvim',
+        run = function()
             pcall(vim.cmd, 'MasonUpdate')
-          end,
-        },
-            {'williamboman/mason-lspconfig.nvim'}, -- Optional
+        end,
+    },
+    {'williamboman/mason-lspconfig.nvim'}, -- Optional
 
-            -- Autocompletion
-            {'hrsh7th/nvim-cmp'},     -- Required
-            {'hrsh7th/cmp-nvim-lsp'}, -- Required
-            {'L3MON4D3/LuaSnip'},     -- Required
-        }
+    -- Autocompletion
+    {'hrsh7th/nvim-cmp'},     -- Required
+    {'hrsh7th/cmp-nvim-lsp'}, -- Required
+    {'L3MON4D3/LuaSnip'},     -- Required
+}
     }
 
     use({
@@ -78,4 +78,7 @@ return require('packer').startup(function(use)
             require("auto-save").setup {}
         end
     })
+    use 'mfussenegger/nvim-dap'
+    use 'leoluz/nvim-dap-go'
+    use { "rcarriga/nvim-dap-ui", requires = {"mfussenegger/nvim-dap"} }
 end)
